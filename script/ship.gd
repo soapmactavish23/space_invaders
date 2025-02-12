0560d030b3e3a57f76351b78b4f05b0de7ef8e83
Henrick <ricknogueira1231@gmail.com>
2025-02-12 16:45:13 -0300

    138. 09 - Criando a nave parte 3 - Limitação nas bordas do jogo

---
+++ script/ship.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+const VEL = 80
+
+var left
+var right
+
+var dir = 0
+
+func _ready():
+	set_process(true)
+	
+func _process(delta):
+	dir = 0
+	
+	right = Input.is_action_pressed("ui_right")
+	left = Input.is_action_pressed("ui_left")
+	
+	if right:
+		dir += 1
+	
+	if left:
+		dir -= 1
+	
+	translate(Vector2(1, 0) * VEL * dir * delta)
+	
+	if get_global_pos().x < 7:
+		set_global_pos(Vector2(7, get_global_pos().y))
+		
+	if get_global_pos().x > 173:
+		set_global_pos(Vector2(173, get_global_pos().y))
