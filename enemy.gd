3988d974d7c156847c972431e9222781ef961971
Henrick <ricknogueira1231@gmail.com>
2025-02-14 20:12:31 -0300

    144. 15 - Criando alienígenas parte 2

--- enemy.gd
+++ enemy.gd
@@ -1,16 +1,25 @@
 extends Area2D
 
-var textures = [
-	preload("res://assets/sprites/InvaderA_sheet.png")
-	preload("res://assets/sprites/InvaderB_sheet.png")
-	preload("res://assets/sprites/InvaderC_sheet.png")
-]
+export (int, "A", "B", "C") var tipo = 0
 
-var scores = [
-	10,
-	20,
-	30
+var score = 0
+
+var atributes = [
+	{
+		texture = preload("res://assets/sprites/InvaderA_sheet.png"), 
+		score = 10
+	},
+	{
+		texture = preload("res://assets/sprites/InvaderB_sheet.png"), 
+		score = 20
+	},
+	{
+		texture = preload("res://assets/sprites/InvaderC_sheet.png"), 
+		score = 30
+	}
 ]
 
 func _ready():
-	pass
+	var atribute = atributes[tipo]
+	get_node("sprite").set_texture(atribute.texture)
+	score = atribute.score
