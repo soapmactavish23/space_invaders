41bc499202eb0ce3562ef3c5d226777f1a684b97
Henrick <ricknogueira1231@gmail.com>
2025-02-14 19:57:23 -0300

    141. 12 - Efetuando disparos lazer

--- script/ship.gd
+++ script/ship.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 const VEL = 80
 
+var prev_shot = preload("res://scenes/ship_shot.tscn")
+
 var left
 var right
 var lazer
+var prev_lazer = false
 
 var dir = 0
 
@@ -31,3 +34,10 @@ func _process(delta):
 		
 	if get_global_pos().x > 173:
 		set_global_pos(Vector2(173, get_global_pos().y))
+	
+	if lazer and not prev_lazer:
+		var shot = prev_shot.instance()
+		get_parent().add_child(shot)
+		shot.set_global_pos(get_global_pos())
+		
+	prev_lazer = lazer
