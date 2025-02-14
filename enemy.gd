708a75f22615539470ffd0ca28bdd7603fd22152
Henrick <ricknogueira1231@gmail.com>
2025-02-14 20:04:54 -0300

    143. 14 - Criando alienígenas parte 1

---
+++ enemy.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var textures = [
+	preload("res://assets/sprites/InvaderA_sheet.png")
+	preload("res://assets/sprites/InvaderB_sheet.png")
+	preload("res://assets/sprites/InvaderC_sheet.png")
+]
+
+var scores = [
+	10,
+	20,
+	30
+]
+
+func _ready():
+	pass
