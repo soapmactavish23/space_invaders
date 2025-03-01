6360bae52df86345835509e4adbf6c57adcc0f1d
Henrick <ricknogueira1231@gmail.com>
2025-03-01 19:00:48 -0300

    171. 42 - Seletor de nomes retro 03

--- script/name_selector.gd
+++ script/name_selector.gd
@@ -5,6 +5,8 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_*# "
 var index = 0
 var letter = 0
 
+signal finished(name)
+
 func _ready():
 	set_process_input(true)
 	
@@ -19,7 +21,24 @@ func _input(event):
 		alterou = true
 	
 	if alterou:
+		if index < 0:
+			index = letters.length() - 1
+		elif index >= letters.length():
+			index = 0
+		
 		get_node("container").get_child(letter).set_text(letters[index])
+	
+	if event.is_action_pressed("disparo") and not event.is_echo():
+		get_node("container").get_child(letter).set_opacity(1)
+		index = 0
+		letter += 1
+		if letter > 2:
+			get_node("timer").stop()
+			set_process_input(false)
+			var name = ""
+			for l in get_node("container").get_children():
+				name += l.get_text()
+			emit_signal("finished", name)
 
 
 func _on_timer_timeout():
