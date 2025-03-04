b01a7cec738c565bdf713be8e345740f77f48d6f
Henrick <ricknogueira1231@gmail.com>
2025-03-04 16:04:00 -0300

    172. 43 - Hiscores 01

--- script/main.gd
+++ script/main.gd
@@ -3,6 +3,19 @@ extends Node
 var pre_game = preload("res://scenes/game.tscn")
 var game
 
+var hiscores = [
+	{name = "AAA", score = 1000},
+	{name = "BBB", score = 900},
+	{name = "CCC", score = 800},
+	{name = "DDD", score = 700},
+	{name = "EEE", score = 600},
+	{name = "FFF", score = 500},
+	{name = "GGG", score = 400},
+	{name = "HHH", score = 300},
+	{name = "III", score = 200},
+	{name = "JJJ", score = 100},
+]
+
 var extra_life_index = 0
 var score = 0
 var lifes = 3
@@ -24,6 +37,11 @@ func _on_Button_pressed():
 	new_game()
 
 func on_game_over():
+	for hs in hiscores:
+		if game.data.score > hs.score:
+			print(hs)
+			break;
+	
 	get_node("btn_new_game").show()
 
 func on_victory():
